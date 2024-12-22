e92f5e579d2b168cb21a3ddae300eacc24d0bd5a
Rodion Smolyar <s1rodion123@gmail.com>
2024-12-22 17:03:28 +0300

    small changes in signals.py

--- films/signals.py
+++ films/signals.py
@@ -1,9 +1,12 @@
 from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
-from .models import Film, Person
+
+from .models import Film, Person, Country, Genre
 
 # храним старые данные
 old_instances = {}
+
+
 # изменения в фильме
 @receiver(pre_save, sender=Film)
 def store_old_instance(sender, instance, **kwargs):
@@ -11,10 +14,11 @@ def store_old_instance(sender, instance, **kwargs):
         # храним старые данные
         old_instances[instance.pk] = sender.objects.get(pk=instance.pk)
 
+
 @receiver(post_save, sender=Film)
 def print_on_save(sender, instance, created, **kwargs):
     if created:
-        print("Создан новый фильм.")
+        print(f"Добавлен новый фильм: {instance.name}")
     else:
         old_instance = old_instances.get(instance.pk)
         if old_instance:
@@ -28,13 +32,16 @@ def print_on_save(sender, instance, created, **kwargs):
                 print(f"Изменены данные фильма")
                 for field in changes:
                     if field != 'updated_at':
-                        print(f"Было: {changes[field]['old']}, стало: {changes[field]['new']}")
+                        print(
+                            f"Было: {changes[field]['old']}, стало: {changes[field]['new']}")
     # удаляем старые данные
     old_instances.pop(instance.pk, None)
 
+
 @receiver(post_delete, sender=Film)
-def print_on_delete(sender, **kwargs):
-    print("Удалили фильм")
+def print_on_delete(sender, instance, **kwargs):
+    print(f"Удалён фильм {instance.name}")
+
 
 # изменения у актёра
 @receiver(pre_save, sender=Person)
@@ -42,10 +49,11 @@ def store_old_instance(sender, instance, **kwargs):
     if instance.pk:
         old_instances[instance.pk] = sender.objects.get(pk=instance.pk)
 
+
 @receiver(post_save, sender=Person)
 def print_on_save(sender, instance, created, **kwargs):
     if created:
-        print("Добавили актёра")
+        print(f"Добавлен новый актёр: {instance.name}")
     else:
         old_instance = old_instances.get(instance.pk)
         if old_instance:
@@ -59,15 +67,37 @@ def print_on_save(sender, instance, created, **kwargs):
                 print("Изменены данные актёра")
                 for field in changes:
                     if field != 'updated_at':
-                        print(f"Было: {changes[field]['old']}, стало: {changes[field]['new']}")
+                        print(
+                            f"Было: {changes[field]['old']}, стало: {changes[field]['new']}")
 
     # удаляем старые данные
     old_instances.pop(instance.pk, None)
 
+
 @receiver(post_delete, sender=Person)
 def print_on_delete(sender, instance, **kwargs):
-    for field in instance._meta.fields:
-        field_name = field.name
-        if field_name == 'name':
-            field_value = getattr(instance, field_name)
-            print(f"Удалили актёра, {field_value}")
+    print(f"Удалили актёра {instance.name}")
+
+
+# добавление/удаление жанра
+@receiver(post_save, sender=Genre)
+def print_on_save(sender, instance, created, **kwargs):
+    if created:
+        print(f"Добавлен новый жанр фильмов: {instance.name}")
+
+
+@receiver(post_delete, sender=Genre)
+def print_on_delete(sender, instance, **kwargs):
+    print(f"Удалён жанр {instance.name}")
+
+
+# добавление/удаление страны
+@receiver(post_save, sender=Country)
+def print_on_save(sender, instance, created, **kwargs):
+    if created:
+        print(f"Теперь можно посмотреть фильмы из страны {instance.name}")
+
+
+@receiver(post_delete, sender=Country)
+def print_on_delete(sender, instance, **kwargs):
+    print(f"Больше нет фильмов из страны {instance.name}")
